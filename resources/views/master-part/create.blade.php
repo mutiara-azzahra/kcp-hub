2fa9403146f64de0e6b6852f522dde95deaf4e3c
mutiara-azzahra <my26recovery@gmail.com>
2023-10-26 16:53:09 +0800

    select2-master kode rak

--- resources/views/master-part/create.blade.php
+++ resources/views/master-part/create.blade.php
@@ -52,28 +52,24 @@
                                     <input type="text" name="satuan_dus" class="form-control" placeholder="contoh: 50">
                                 </div>
                             </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <strong>Kode Grup</strong>
-                                    <select name="id_grup" class="form-control" >
-                                        <option value="">---Pilih Group Part--</option>
-                                        <option value="1">Ichidai</option>
-                                        <option value="2">Brio</option>
-                                        <option value="3">Group Air Aki</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <strong>Kode Rak</strong>
-                                    <select name="id_rak" class="form-control" >
-                                        <option value="">---Pilih Kode Rak--</option>
-                                        @foreach($kode_rak as $a)
-                                            <option value="{{ $a->id }}">{{ $a->kode_rak_lokasi }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
+                                   
+                                        <strong>Kode Grup</strong><br>
+                                        <select name="id_grup" class="form-control my-select" >
+                                            <option value="">---Pilih Group Part--</option>
+                                            <option value="1">Ichidai</option>
+                                            <option value="2">Brio</option>
+                                            <option value="3">Group Air Aki</option>
+                                        </select><br><br>
+
+                                        <strong>Kode Rak</strong><br>
+                                        <select name="id_rak" class="form-control my-select" >
+                                            <option value="">---Pilih Kode Rak--</option>
+                                            @foreach($kode_rak as $a)
+                                                <option value="{{ $a->id }}">{{ $a->kode_rak_lokasi }}</option>
+                                            @endforeach
+                                        </select><br><br>
+                                        
+
                             <div class="col-xs-12 col-sm-12 col-md-12 text-center">
                                 <div class="float-right">
                                     <button type="submit" class="btn btn-success"><i class="fas fa-save"></i> Simpan Data</button>                            
