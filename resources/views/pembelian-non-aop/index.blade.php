22171a94638f54092ddd4d51d5049b5c4aa4b2cf
mutiara-azzahra <my26recovery@gmail.com>
2023-10-26 08:59:29 +0800

    view-2

--- resources/views/pembelian-non-aop/index.blade.php
+++ resources/views/pembelian-non-aop/index.blade.php
@@ -2,7 +2,7 @@
  
 @section('content')
 <div class="container" style="padding: 10px;">
-    <div class="row mt-5">
+    <div class="row mt-2">
         <div class="col-lg-12 pb-3">
              <div class="float-left">
                 <h4><b>PEMBELIAN NON AOP</b></h4>
