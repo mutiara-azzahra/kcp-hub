92af5244b94cb01adfd52bda474f7e9d609a10e5
mutiara-azzahra <my26recovery@gmail.com>
2023-11-22 16:46:51 +0800

    lss-selesai

--- app/Http/Controllers/ReportLssController.php
+++ app/Http/Controllers/ReportLssController.php
@@ -35,58 +35,66 @@ class ReportLssController extends Controller
         $date               = Carbon::create(null, $bulan, 1, 0, 0, 0);
         $previousMonth      = $date->subMonth()->month;
 
-        if($previousMonth = 10 && $tahun = 2023 ){
-            $awal_amount = 0;
-        }
 
-        $getLevel4 = MasterLevel4::all();
+        $lss = LSS::where('bulan', $bulan)->where('tahun', $tahun)->first();
 
-        foreach($getLevel4 as $i){
-
-            $getBeli = InvoiceNonHeader::where('created_at', '>=', $tahun.'-'.$bulan.'-01')
-            ->where('created_at', '<=', $tahun.'-'.$bulan.'-'.Carbon::createFromDate($tahun, $bulan, 1)->endOfMonth()->format('d'))
-            ->get();
-
-            $getHpp = TransaksiInvoiceDetails::where('created_at', '>=', $tahun.'-'.$bulan.'-01')
-                ->where('created_at', '<=', $tahun.'-'.$bulan.'-'.Carbon::createFromDate($tahun, $bulan, 1)->endOfMonth()->format('d'))
-                ->get();
-
-            $getModalTerjual = ModalPartTerjual::where('created_at', '>=', $tahun.'-'.$bulan.'-01')
-                ->where('created_at', '<=', $tahun.'-'.$bulan.'-'.Carbon::createFromDate($tahun, $bulan, 1)->endOfMonth()->format('d'))
-                ->get();
-
-            $part       = MasterPart::where('level_2', $i->id_level_2)->where('level_4', $i->level_4)->pluck('part_no')->toArray();
-            $flattened   = collect($part)->flatten()->toArray();
-
-            $beli = 0;
-
-            foreach($getBeli as $s){
-                $beli += $s->details_pembelian->whereIn('part_no', $flattened)->sum('total_amount');
+        if($lss == null){
+            if($previousMonth = 10 && $tahun = 2023 ){
+                $awal_amount = 0;
             }
-
-            $hpp     = $getHpp->whereIn('part_no', $flattened)->sum('nominal_total')/1.11;
-            $jual    = $getModalTerjual->whereIn('part_no', $flattened)->sum('nominal_modal')/1.11;
-
-            //INSERT LSS TO DB
-            $value = [
-                'bulan'                 => $bulan,
-                'tahun'                 => $tahun,
-                'sub_kelompok_part'     => $i->level_4,
-                'produk_part'           => $i->id_level_2,
-                'awal_amount'           => $awal_amount,
-                'beli'                  => $beli,
-                'jual_rbp'              => $hpp,
-                'jual_dbp'              => $jual,
-                'akhir_amount'          => ($awal_amount + $beli) - $jual,
-                'status'                => 'A',
-                'created_at'            => NOW(),
-                'created_by'            => Auth::user()->nama_user,
-            ];
-
-            $created = LSS::create($value);
-
+    
+            $getLevel4 = MasterLevel4::all();
+    
+            foreach($getLevel4 as $i){
+    
+                $getBeli = InvoiceNonHeader::where('created_at', '>=', $tahun.'-'.$bulan.'-01')
+                ->where('created_at', '<=', $tahun.'-'.$bulan.'-'.Carbon::createFromDate($tahun, $bulan, 1)->endOfMonth()->format('d'))
+                ->get();
+    
+                $getHpp = TransaksiInvoiceDetails::where('created_at', '>=', $tahun.'-'.$bulan.'-01')
+                    ->where('created_at', '<=', $tahun.'-'.$bulan.'-'.Carbon::createFromDate($tahun, $bulan, 1)->endOfMonth()->format('d'))
+                    ->get();
+    
+                $getModalTerjual = ModalPartTerjual::where('created_at', '>=', $tahun.'-'.$bulan.'-01')
+                    ->where('created_at', '<=', $tahun.'-'.$bulan.'-'.Carbon::createFromDate($tahun, $bulan, 1)->endOfMonth()->format('d'))
+                    ->get();
+    
+                $part       = MasterPart::where('level_2', $i->id_level_2)->where('level_4', $i->level_4)->pluck('part_no')->toArray();
+                $flattened   = collect($part)->flatten()->toArray();
+    
+                $beli = 0;
+    
+                foreach($getBeli as $s){
+                    $beli += $s->details_pembelian->whereIn('part_no', $flattened)->sum('total_amount');
+                }
+    
+                $hpp     = $getHpp->whereIn('part_no', $flattened)->sum('nominal_total')/1.11;
+                $jual    = $getModalTerjual->whereIn('part_no', $flattened)->sum('nominal_modal')/1.11;
+    
+                //INSERT LSS TO DB
+                $value = [
+                    'bulan'                 => $bulan,
+                    'tahun'                 => $tahun,
+                    'sub_kelompok_part'     => $i->level_4,
+                    'produk_part'           => $i->id_level_2,
+                    'awal_amount'           => $awal_amount,
+                    'beli'                  => $beli,
+                    'jual_rbp'              => $hpp,
+                    'jual_dbp'              => $jual,
+                    'akhir_amount'          => ($awal_amount + $beli) - $jual,
+                    'status'                => 'A',
+                    'created_at'            => NOW(),
+                    'created_by'            => Auth::user()->nama_user,
+                ];
+    
+                $created = LSS::create($value);
+    
+            }
         }
 
+        $data = LSS::where('bulan', $bulan)->where('tahun', $tahun)->get();
+    
+        return view('report-lss.view', compact('data', 'bulan', 'tahun'));
 
     }
 }
--- resources/views/surat-pesanan/index.blade.php
+++ resources/views/surat-pesanan/index.blade.php
@@ -12,47 +12,48 @@
             </div>
         </div>
     </div>
-            @if ($message = Session::get('success'))
-                <div class="alert alert-success" id="myAlert">
-                    <p>{{ $message }}</p>
-                </div>
-            @elseif ($message = Session::get('danger'))
-                <div class="alert alert-danger" id="myAlert">
-                    <p>{{ $message }}</p>
-                </div>
-            @endif
 
-            <div class="card" style="padding: 10px;">
-                <div class="card-body">
-                    <div class="col-lg-12">  
-                        <table class="table table-hover table-bordered table-sm bg-light table-striped" id="example2">
-                            <thead>
-                                <tr style="background-color: #6082B6; color:white">
-                                    <th class="text-center">SP</th>
-                                    <th class="text-center">Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @php
-                                $no=1;
-                                @endphp
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success" id="myAlert">
+            <p>{{ $message }}</p>
+        </div>
+    @elseif ($message = Session::get('danger'))
+        <div class="alert alert-danger" id="myAlert">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
 
-                                @foreach($surat_pesanan as $p)
-                                <tr>
-                                    <td class="text-center">{{ $p->nosp }}</td>
-                                    <td class="text-center">
-                                        <a class="btn btn-info btn-sm" href="{{ route('surat-pesanan.detail', $p->nosp) }}">
-                                            <i class="fas fa-eye"></i>
-                                        </a>
-                                    </td>
-                                </tr>
-                                @endforeach
-                                
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+    <div class="card" style="padding: 10px;">
+        <div class="card-body">
+            <div class="col-lg-12">  
+                <table class="table table-hover table-bordered table-sm bg-light table-striped" id="example2">
+                    <thead>
+                        <tr style="background-color: #6082B6; color:white">
+                            <th class="text-center">SP</th>
+                            <th class="text-center">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                        $no=1;
+                        @endphp
+
+                        @foreach($surat_pesanan as $p)
+                        <tr>
+                            <td class="text-center">{{ $p->nosp }}</td>
+                            <td class="text-center">
+                                <a class="btn btn-info btn-sm" href="{{ route('surat-pesanan.detail', $p->nosp) }}">
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        @endforeach
+                        
+                    </tbody>
+                </table>
             </div>
+        </div>
+    </div>
 </div>
 @endsection
 
