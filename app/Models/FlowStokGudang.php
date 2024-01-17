bbf07e55050e885d877bbeaa0651ce1dec959039
mutiara-azzahra <my26recovery@gmail.com>
2024-01-17 09:19:27 +0800

    flow stok gudang

---
+++ app/Models/FlowStokGudang.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FlowStokGudang extends Model
+{
+    use HasFactory;
+
+    protected $table = 'flow_stok_gudang';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'part_no',
+        'tanggal_barang_masuk',
+        'tanggal_barang_keluar',
+        'stok_awal',
+        'stok_masuk',
+        'stok_keluar',
+        'stok_akhir',
+        'created_at',
+        'created_by',
+        'updated_at',
+        'updated_by'
+    ];
+
+}
