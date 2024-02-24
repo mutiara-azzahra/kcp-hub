27fd44f1aeef6d94797fc9d94181d1ade5d543bf
mutiara-azzahra <my26recovery@gmail.com>
2024-02-24 09:55:57 +0800

    transaksi akuntansi

--- app/Models/TransaksiAkuntansiJurnalDetails.php
+++ app/Models/TransaksiAkuntansiJurnalDetails.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class TransaksiAkuntansiJurnalDetails extends Model
 {
     use HasFactory;
+
+    protected $table = 'transaksi_akuntansi_jurnal_header';
+    public $primaryKey = 'id';
+
+    protected $fillable = [
+        'id_header', 
+        'perkiraan',
+        'keterangan',
+        'kategori',
+        'debet',
+        'kredit',
+        'status',
+        'created_at',
+        'created_by',
+        'updated_at',
+        'updated_by'
+    ];
+
+    public function details()
+    {
+        return $this->hasMany(TransaksiAkuntansiJurnalDetails::class, 'id', 'id_header');
+    }
 }
--- app/Models/TransaksiAkuntansiJurnalHeader.php
+++ app/Models/TransaksiAkuntansiJurnalHeader.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class TransaksiAkuntansiJurnalHeader extends Model
 {
     use HasFactory;
+
+    protected $table = 'transaksi_akuntansi_jurnal_header';
+    public $primaryKey = 'id';
+
+    protected $fillable = [
+        'trx_date', 
+        'trx_from',
+        'keterangan',
+        'catatan',
+        'kategori',
+        'flag_batal',
+        'keterangan_batal',
+        'status',
+        'created_at',
+        'created_by',
+        'updated_at',
+        'updated_by'
+    ];
+
+    public function details()
+    {
+        return $this->hasMany(TransaksiAkuntansiJurnalDetails::class, 'id', 'id_header');
+    }
 }
